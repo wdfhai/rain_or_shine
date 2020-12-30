20afa391dde9b4de65c7ef956b6553b2b5adefeb
wdfhai <wd.fhai@outlook.com>
2020-12-30 13:42:51 -0600

    added search function with 2 ajax calls

--- develop/script.js
+++ develop/script.js
@@ -7,21 +7,51 @@ $("#forecastFourDate")[0].textContent = moment().add(4, 'd').format('ddd, MMM DD
 $("#forecastFiveDate")[0].textContent = moment().add(5, 'd').format('ddd, MMM DD');
 
 var currentCity = "";
+var searchedCity = "";
 var previousCities = [];
+var previousCitiesList = $("#previousCities")[0];
+var previousCitiesButtons = $(".button")[0];
 
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=0d98e0477d04a50c6eee875a618c9c74"
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=0d98e0477d04a50c6eee875a618c9c74"
 
 
 function search(event){
     event.preventDefault();
     var searchedCity = $(".form-control")[0].value;
     if (!(searchedCity === "")){
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=0d98e0477d04a50c6eee875a618c9c74",
+            method: "GET"
+            })
+            .then(function(response) {
+                console.log(response);
+                var lon = parseInt(response.coord.lon);
+                var lat = parseInt(response.coord.lat);
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=0d98e0477d04a50c6eee875a618c9c74",
+                method: "GET"
+                })
+                .then(function(responseOneCall) {
+                    console.log(responseOneCall);
+                    $("#cityName").html(response.name);
+                    $("#cityLocation").html(response.sys.country);
+                    var tempK = parseInt(responseOneCall.current.temp);
+                    var tempC = Math.round(((tempK - 273.15)*(9/5))+32);
+                    $("#currentTemp").text("Temperature " + tempC + " °C");
+                    $("#currentWindSpeed").text("Wind Speed: " + responseOneCall.current.wind_speed + " mph");
+                    $("#currentHumidity").text("Humidity: " + responseOneCall.current.humidity + " %");
+                    $("#currentUvIndex").text("UV Index: " + responseOneCall.current.uvi);
+                })
+            });
     previousCities.push(searchedCity);
-    console.log(searchedCity);
-    console.log(previousCities);
+    var test = $("<button/>", {"class":"button", type:"button", "id":"previousCity"} );
+    test[0].textContent = searchedCity;
+    previousCitiesList.append(test[0]);
+    $(".form-control")[0].value = "";
     } else {
-        alert("You need to enter a city name");
+        alert("You need to enter a city name.");
     };
 }
 
 $("#searchBtn").on('click', search);
+$("#previousCity").on('click', console.log('previous'));
