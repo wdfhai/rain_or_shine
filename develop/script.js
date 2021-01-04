04e1f2a78c82127a3f9d431ee36232d63632a551
wdfhai <wd.fhai@outlook.com>
2021-01-03 22:23:12 -0600

    made previous search buttons functional

--- develop/script.js
+++ develop/script.js
@@ -3,20 +3,55 @@ var currentCity = "";
 var searchedCity = "";
 var previousCities = [];
 var previousCitiesList = $("#previousCities")[0];
-var previousCitiesButtons = $(".button")[0];
+var previousCitiesButtons = $("#previousCitiesButtons");
+var storedCities = [];
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=0d98e0477d04a50c6eee875a618c9c74"
 
-function search(event){
-    event.preventDefault();
-    var searchedCity = $(".form-control")[0].value;
-    if (!(searchedCity === "")){
-        $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=0d98e0477d04a50c6eee875a618c9c74",
-            method: "GET"
-            })
-            .then(function(response) {
-                console.log(response);
+function previousSearches(){
+    if (storedCities.length > 0){
+        previousCitiesButtons.empty();
+        storedCities.forEach(function(item){
+            var dynBtn = $("<div/>", {"class":"button btn-lg mb-2", type:"button", "id":"previousCity"});
+            dynBtn[0].textContent = item;
+            previousCitiesButtons.append(dynBtn);
+        })
+    } else {
+        console.log('no previous searches');
+    }
+}
+
+function search(){
+    var searchedCity = ($(".form-control")[0].value).toUpperCase();
+    console.log(searchedCity)
+    if (searchedCity === ""){
+        alert("You need to enter a city name.");
+    } else {
+        if (!(storedCities.includes(searchedCity))){
+            storedCities.push(searchedCity);
+        } else {
+            alert('City previously searched');
+            $(".form-control")[0].value = "";
+            return;
+        };
+        console.log(storedCities);
+        previousSearches();
+        $(".form-control")[0].value = "";
+        ajax(searchedCity);
+    };
+};
+
+
+
+function ajax(searchedCity){
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=0d98e0477d04a50c6eee875a618c9c74",
+        method: "GET"
+        })
+        .then(function(response) {
+            if (response === null){
+                alert("not found")
+            } else {
                 var lon = parseInt(response.coord.lon);
                 var lat = parseInt(response.coord.lat);
             $.ajax({
@@ -24,14 +59,12 @@ function search(event){
                 method: "GET"
                 })
                 .then(function(responseOneCall) {
-                    console.log(responseOneCall);
                     $("#cityName").html(response.name);
                     $("#cityLocation").html(response.sys.country);
                     var tempK = parseInt(responseOneCall.current.temp);
                     var tempC = Math.round(((tempK - 273.15)*(9/5))+32);
                     var icon = responseOneCall.current.weather[0].icon;
                     var utcOffset = ((responseOneCall.timezone_offset)/60);
-                    console.log(utcOffset);
                     $("#currentDateAndTime")[0].textContent = moment().utcOffset(utcOffset).format('LLLL');
                     $("#currentWeather").attr("src", "http://openweathermap.org/img/wn/"+icon+"@2x.png");
                     $("#currentTemp").text("Temperature " + tempC + " °F");
@@ -41,21 +74,22 @@ function search(event){
                     var uviBtn = $("<button/>", {"class":"btn-sm", type:"button", "id":"uviValueBtn"});
                     uviBtn[0].textContent = uviValue;
                     uviBtn[0].disabled = true;
+                    $("#currentUvIndex").empty();
                     if (uviValue <= 2){
                         uviBtn[0].style.backgroundColor = 'lightgreen';
-                        $("#currentUvIndex").append(uviBtn[0]);
+                        $("#currentUvIndex").append("UV Index: ",uviBtn[0]);
                     } else if ((uviValue >= 2) && (uviValue <= 5)) {
                         uviBtn[0].style.backgroundColor = 'yellow';
-                        $("#currentUvIndex").append(uviBtn[0]);
+                        $("#currentUvIndex").append("UV Index: ",uviBtn[0]);
                     } else if ((uviValue >= 6) && (uviValue <= 7)){
                         uviBtn[0].style.backgroundColor = 'orange';
-                        $("#currentUvIndex").append(uviBtn[0]);
+                        $("#currentUvIndex").append("UV Index: ",uviBtn[0]);
                     } else if ((uviValue >= 8) && (uviValue <= 10)) {
                         uviBtn[0].style.backgroundColor = 'red';
-                        $("#currentUvIndex").append(uviBtn[0]);
+                        $("#currentUvIndex").append("UV Index: ",uviBtn[0]);
                     } else {
                         uviBtn[0].style.backgroundColor = 'violet';
-                        $("#currentUvIndex").append(uviBtn[0]);
+                        $("#currentUvIndex").append("UV Index: ",uviBtn[0]);
                     };
                     $("#fiveDayForecast")[0].style.display = "flex";
                     $("#forecastOneDate")[0].textContent = moment().utcOffset(utcOffset).add(1, 'd').format('ddd, MMM DD');
@@ -79,26 +113,18 @@ function search(event){
                     $("#forecastFiveTemp").text("Temp: Max: " + Math.round(((responseOneCall.daily[4].temp.max - 273.15)*(9/5))+32) + "°F, Min: " + Math.round(((responseOneCall.daily[4].temp.min - 273.15)*(9/5))+32) + "°F");
                     $("#forecastFiveHumidity").text("Humidity: " + responseOneCall.daily[4].humidity + "%");
                 })
-            });
-    previousCities.push(searchedCity);
-    $("#previousContainer")[0].style.display = "block";
-    var dynBtn = $("<div/>", {"class":"button btn-info btn-lg mb-2", type:"button", "id":"previousCity"});
-    dynBtn[0].textContent = searchedCity;
-    previousCitiesList.append(dynBtn[0]);
-    $(".form-control")[0].value = "";
-    // icons();
-    } else {
-        alert("You need to enter a city name.");
-    };
-}
+            };
+        })
+};
 
-// function icons (){
-//     var cW = $("#currentWeather")[0];
-//     var cWT = cW.textContent;
-//     var cWTA = cWT.split("");
-//     console.log(cWT);
-//     console.log(cWTA);
-// }
+
+function repeatSearch (){
+    const searchedCity = $(this)[0].textContent;
+    console.log(searchedCity);
+    ajax(searchedCity);
+};
+
 
 $("#searchBtn").on('click', search);
-$("#previousCity").on('click', console.log('previous'));
+
+$('body').on('click', '.btn-lg', repeatSearch);
